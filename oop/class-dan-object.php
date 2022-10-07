1c7a89cee76a2a908739a742093fe5d395ce8dad
Agil_Alatas <agilalatas17@gmail.com>
2022-10-07 15:20:08 +0700

    oop Class dan Object

---
+++ oop/class-dan-object.php
@@ -0,0 +1,19 @@
+<?php
+    // Membuat Class
+    /* diawali dengan keyword "class" dan dibatasi dengan "{}" */
+
+    class ClassDanObject {
+
+    }
+
+
+    // Instance Object
+    $a = new ClassDanObject();
+    $b = new ClassDanObject();
+    $c = new ClassDanObject();
+
+    var_dump($a);
+    var_dump($b);
+    var_dump($c);
+
+?>
