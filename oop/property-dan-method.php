d380d6ae98d966d413a25608da09033d67ed3a5d
Agil_Alatas <agilalatas17@gmail.com>
2022-10-07 16:50:57 +0700

    Property dan Method

---
+++ oop/property-dan-method.php
@@ -0,0 +1,15 @@
+<?php
+
+// Property
+/*
+    - Merepresentasikan data atau keadaan dari sebuah object
+    - Variabel yang ada di dalam object (member variabel)
+    - sama seperti variabel didalam PHP, ditambah dengan visibility didepannya
+*/
+
+// Method
+/*
+    - Merepresentasikan perilaku dari sebuah object
+    - Function yang ada di dalam object
+    - sama seperti function di PHP, ditambah dengan visibility didepanya
+*/
