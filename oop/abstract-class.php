850162e818512aad9d348e5e1e6ad0342bb75b68
Agil_Alatas <agilalatas17@gmail.com>
2022-10-09 15:31:11 +0700

    Class Abstract (Bagian 1)

---
+++ oop/abstract-class.php
@@ -0,0 +1,23 @@
+<?php
+
+// Abstract Class
+/*
+    - Sebuah kelas yang tidak dapat di instansiasi.
+    - Kelas 'abstrak'.
+    - Mendefinisikan interface untuk kelas lain yang menjadi turunannya.
+    - berperan sebagai 'kerangka dasar' untuk kelas turunannya.
+    - Memiliki minimal 1 method abstrak.
+    - Digunakan dalam 'pewarisan' / inheritance untuk 'memaksakan' implementasi method abstrak yang sama untuk semua kelas turunannya.
+    - Semua kelas turunan, harus mengimplementasikan method abstrak yang ada di kelas abstraknya.
+    - Kelas abstrak boleh memiliki property / method regular.
+    - Kelas abstrak boleh memiliki property / static method.
+*/
+
+// Kenapa Menggunakan Kelas Abstrak ?
+/*
+    - Merepresentasikan ide atau konsep dasar.
+    - "Composition Over Inheritance"
+    - Salah satu cara menerapkan Polimorphism
+    - Sentralisasi Logic
+    - Mempermudah pengerjaan tim
+*/
