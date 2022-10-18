91eaa1a67d0c56d27b335ffd7cf8673975cf2a75
Agil_Alatas <agilalatas17@gmail.com>
2022-10-18 23:50:15 +0700

    Model

---
+++ mvc/app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model {
+    private $nama = 'Agil Alatas';
+
+    public function getUser() {
+        return $this->nama;
+    }
+}
--- mvc/app/views/templates/header.php
+++ mvc/app/views/templates/header.php
@@ -13,24 +13,19 @@
 
 <body>
 
-    <div class="container">
-        <nav class="navbar navbar-expand-lg bg-info">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home <span
-                                class="sr-only">(current)</span></a>
-                        <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
-                    </div>
+    <nav class="navbar navbar-expand-lg bg-info">
+        <div class="container">
+            <a class="navbar-brand" href="<?= BASEURL; ?>">PHP MVC</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="<?= BASEURL; ?>">Home</a>
+                    <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
+                    <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
                 </div>
             </div>
-        </nav>
-
-
-    </div>
+        </div>
+    </nav>
