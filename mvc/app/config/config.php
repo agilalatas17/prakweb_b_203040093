67754dfcdfdaf7ad610d628fed797cce9e396d78
Agil_Alatas <agilalatas17@gmail.com>
2022-10-19 00:35:11 +0700

    Database Wrapper

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_b_203040093/mvc/public');
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -3,17 +3,16 @@
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
 
-            <ul>
+            <ul class="list-group">
                 <?php foreach($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?= $mhs['nama']; ?></li>
-                    <li><?= $mhs['nrp']; ?></li>
-                    <li><?= $mhs['email']; ?></li>
-                    <li><?= $mhs['jurusan']; ?></li>
-                </ul>
-                <br>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <?= $mhs['nama']; ?>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>"
+                        class="badge text-bg-info stretched-link">Detail</a>
+                </li>
                 <?php endforeach; ?>
             </ul>
+
         </div>
     </div>
 </div>
