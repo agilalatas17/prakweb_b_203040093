c25d71e80986957b2ba98a2bb1a821f38fd990be
Agil_Alatas <agilalatas17@gmail.com>
2022-10-20 15:34:54 +0700

    Insert Data + Modal Box Bootstrap

--- mvc/app/views/mahasiswa/detail.php
+++ mvc/app/views/mahasiswa/detail.php
@@ -5,7 +5,7 @@
             <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nrp']; ?></h6>
             <p class="card-text"><?= $data['mhs']['email']; ?></p>
             <p class="card-text"><?= $data['mhs']['jurusan']; ?></p>
-            <a href="<?= BASEURL; ?>/mahasiswa" class="card-link">Kembali</a>
+            <a href="<?= BASEURL; ?>/mahasiswa" class="btn text-primary card-link">Kembali</a>
         </div>
     </div>
 </div>
